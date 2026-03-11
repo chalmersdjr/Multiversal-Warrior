67d65a1906cbc68006e9c283791c16efe59895be
chalmersdjr <57692260+chalmersdjr@users.noreply.github.com>
2026-03-10 20:48:32 -0400

    Revamp fullRevamp.js file

--- fullRevamp.js
+++ fullRevamp.js
@@ -79,9 +79,9 @@ class GameData {
     this.lastTick = options.lastTick || Date.now();
     this.tickSpeedOff = options.tickSpeedOff || 0;
     //tickspeed mult temp
-    this.tickSpeedMult = options.tickSpeedMult || 1;
-    this.tickSpeed = options.tickSpeed || 1;
-    this.baseTickSpeed = options.baseTickSpeed || 0.0001
+    this.tickSpeedMult = options.tickSpeedMult || 100;
+    this.tickSpeed = options.tickSpeed || 100;
+    this.baseTickSpeed = options.baseTickSpeed || 10
   }
 }
 
@@ -2373,7 +2373,7 @@ class UniversalIn {
     options = options || {};
 
     this.universe = options.universe || 1;
-    this.ascensionPoint = options.ascensionPoint || 0;
+    this.ascensionPoint = options.ascensionPoint || 100;
     this.ascensionPointMax = options.ascensionPointMax || 0;
 
     this.milestones = options.milestones || {
